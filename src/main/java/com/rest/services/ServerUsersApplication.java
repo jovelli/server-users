715ca92e23394937db19bc7087d98dde062556f9
Romulo <romulo8000@gmail.com>
2017-12-05 13:54:54 -0200

    Added internationalization

--- src/main/java/com/rest/services/ServerUsersApplication.java
+++ src/main/java/com/rest/services/ServerUsersApplication.java
@@ -2,6 +2,11 @@ package com.rest.services;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import java.util.Locale;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.context.support.ResourceBundleMessageSource;
 
 @SpringBootApplication
 public class ServerUsersApplication {
@@ -9,4 +14,19 @@ public class ServerUsersApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServerUsersApplication.class, args);
 	}
+
+	@Bean
+	public LocaleResolver getLocaleResolver() {
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
+
+	@Bean
+	public ResourceBundleMessageSource messageSource() {
+		ResourceBundleMessageSource source = new ResourceBundleMessageSource();
+		source.setBasename("messages");
+		return source;
+	}
+
 }
--- src/main/java/com/rest/services/controllers/UsersController.java
+++ src/main/java/com/rest/services/controllers/UsersController.java
@@ -2,10 +2,12 @@ package com.rest.services.controllers;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 import javax.validation.Valid;
 import com.rest.services.beans.User;
 import com.rest.services.dao.UserDao;
 import com.rest.services.exceptions.UserNotFoundException;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +21,9 @@ public class UsersController {
 
     @Autowired
     private UserDao userDao;
+
+    @Autowired
+    private MessageSource messageSource;
 
     @GetMapping(path="/users-bean")
     public User userBean() {
@@ -72,6 +77,12 @@ public class UsersController {
         ).orElseThrow(
             () -> new UserNotFoundException("User doesn't exist")
         );
+    }
+
+    @GetMapping(path = "/hello-world-internationalized")
+    public String helloWorldInternationalized(
+            @RequestHeader(name="Accept-Language", required=false) Locale locale) {
+        return messageSource.getMessage("good.morning.message", null, locale);
     }
 
 }
